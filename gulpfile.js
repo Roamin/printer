872661743e004e61b4f9a3c9f22fe6aa131b3256
color vivid <color vivid>
2017-04-27 19:57:53 +0800

    cache

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 /**
- * Created by ivill on 2017/4/5.
+ * Created by Live on 2017/4/5.
  */
 
+const del = require('del');
 const gulp = require('gulp');
-const template = require('gulp-template');
+const markdown = require('gulp-markdown');
 
-gulp.task('default', () => {
-    gulp.src('input/doc.html')
-        .pipe( template({name: 'Sindre'}) )
-        .pipe(gulp.dest('dist'));
-}
+// clear old files
+gulp.task('clean', () => {
+    del([
+        'output/*'
+    ]);
+});
+
+gulp.task('convert', () => {
+    return gulp.src('input/*.md')
+        .pipe(markdown())
+        .pipe(gulp.dest('output/'));
+});
+
+gulp.task('default', ['clean', 'convert']);
--- input/doc.md
+++ input/doc.md
@@ -1,4 +1,12 @@
-## BakeDoc
+# marked
 
-Baking markdown document.
+> A full-featured markdown parser and compiler, written in JavaScript. Built
+> for speed.
 
+[![NPM version](https://badge.fury.io/js/marked.png)][badge]
+
+## Install
+
+``` bash
+npm install marked --save
+```
